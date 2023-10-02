a8af23b37edcf45dcb734c7c677156d1298807aa
Tushar Kumar <tusharkumar0807@gmail.com>
2023-10-02 14:51:54 +0530

    Home screen update with new functionality feature

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,32 +1,31 @@
 import { View, Text, SafeAreaView, StatusBar, Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect, useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-// import Categories from '../components/categories'
-// import FeatureRow from '../components/featuredRow'
-// import { getFeaturedResturants } from '../api';
-// import * as Icon from "react-native-feather";
-// import { themeColors } from '../theme'
+import Categories from '../components/categories'
+import FeatureRow from '../components/featuredRow'
+import { getFeaturedResturants } from '../api';
+import * as Icon from "react-native-feather";
+import { themeColors } from '../theme'
 
 export default function HomeScreen() {
 
-    // const [featuredCategories, setFeaturedCategories] = useState([])
-    // const navigation = useNavigation();
-    // useLayoutEffect(() => {
-    //   navigation.setOptions({headerShown: false})
-    // }, [])
-    // useEffect(()=>{
-    //     getFeaturedResturants().then(data=>{
-    //         setFeaturedCategories(data);
-    //     })
-    // },[])
+    const [featuredCategories, setFeaturedCategories] = useState([])
+    const navigation = useNavigation();
+    useLayoutEffect(() => {
+      navigation.setOptions({headerShown: false})
+    }, [])
+    useEffect(()=>{
+        getFeaturedResturants().then(data=>{
+            setFeaturedCategories(data);
+        })
+    },[])
 
   return (
     <SafeAreaView className="bg-white" >
-        <Text>Home area</Text>
-    {/* <StatusBar
+    <StatusBar
         barStyle="dark-content" 
-    /> */}
-        {/* <View className="flex-row items-center space-x-2 px-4 pb-2 ">
+    />
+        <View className="flex-row items-center space-x-2 px-4 pb-2 ">
             <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
                 <Icon.Search height="25" width="25" stroke="gray" />
                 <TextInput placeholder='Resturants' className="ml-2 flex-1" keyboardType='default' />
@@ -38,11 +37,11 @@ export default function HomeScreen() {
             <View style={{backgroundColor: themeColors.bgColor(1)}} className="p-3 rounded-full">
                 <Icon.Sliders height={20} width={20} strokeWidth="2.5" stroke="white" />
             </View>
-        </View> */}
+        </View>
 
          
 
-    {/* <ScrollView
+    <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{
             paddingBottom: 50
@@ -72,7 +71,7 @@ export default function HomeScreen() {
 
         
        
-    </ScrollView> */}
+    </ScrollView>
       
     </SafeAreaView>
   )
